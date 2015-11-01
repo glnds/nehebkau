b2d7107428ce4ca5375a5ad95f3fb1add24cb4dd
Gert Leenders <leenders.gert@gmail.com>
2015-11-01 11:52:15 +0100

    Initial commit

---
+++ kickoff.py
@@ -0,0 +1,6 @@
+def main():
+    import ipdb; ipdb.set_trace()
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
---
+++ route53DynDns.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+"""
+pip install dnspython ipify
+"""
+
+import ipify
+import pdb; pdb.set_trace()  # XXX BREAKPOINT
+import datetime
+
+import pdb; pdb.set_trace()  # XXX BREAKPOINT
+ip = ipify.get_ip()
+print(ip)
