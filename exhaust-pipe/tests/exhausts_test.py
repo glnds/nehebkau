65a76af933deacbd28dcc8d4ba9d79547cbd8dc1
Gert Leenders <leenders.gert@gmail.com>
2016-04-22 11:24:16 +0200

    Refactoring

---
+++ exhaust-pipe/tests/exhausts_test.py
@@ -0,0 +1,11 @@
+import unittest
+import exhaust-pipe import read_config
+
+class TestExhaustPipe(unittest.TestCase):
+    """
+    Exhaust pipe test class
+    """
+
+    def test_read_config(self):
+        json = read_config
+
