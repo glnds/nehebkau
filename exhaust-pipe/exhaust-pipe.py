274b02efc2c4108058fca5f0483c8acb01882365
Gert Leenders <leenders.gert@gmail.com>
2016-04-25 22:14:10 +0200

    Added some new emissions

--- exhaust-pipe/exhaust-pipe.py
+++ exhaust-pipe/exhaust-pipe.py
@@ -5,6 +5,7 @@ import yaml
 import random
 import struct
 import socket
+import uuid
 
 from randomdate import RandomDate
 
@@ -41,27 +42,49 @@ def read_movies():
 def main():
 
     config = read_config()
-    print(config)
 
     locations = read_edgelocations()
     sizel = len(locations)
 
     movies = read_movies()
-    print(len(movies))
+    sizem = len(movies)
 
     rd = RandomDate('2016-01-01', '2016-04-30')
-    print(rd.random())
 
     with open('cf.log', 'w') as stream:
-        # date and time
-        line = rd.random()
-        # x-edge-location
-        line += '\t' + locations[random.randint(0, sizel-1)]
-        # sc-bytes
-        line += '\t' + random.randint(1024, 262144000)
-        # c-ip
-        socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
-        stream.write(line)
+        for _ in range(75000):
+            # date and time
+            line = rd.random()
+            # x-edge-location
+            line += '\t' + locations[random.randint(0, sizel-1)]
+            # sc-bytes
+            line += '\t' + str(random.randint(1024, 262144000))
+            # c-ip
+            line += '\t' + socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
+            # cd-method
+            line += '\tGET'
+            # cs host
+            line += '\td111111abcdef8.cloudfront.net'
+            # cs-uri-stem
+            slug = movies[random.randint(0, sizem-1)]
+            line += '\t/movies/' + slug + '/dummy.mp4'
+            # cs-status
+            line += '\t200'
+            # cs referer
+            line += '\thttps://www.example.com/movies/' + slug
+            # cs user-agent
+            line += '\tMozilla/5.0%2520(Windows%2520NT%25206.3;%2520WOW64)%2520AppleWebKit/537.36' \
+                    '%2520(KHTML,%2520like%2520Gecko)%2520Chrome/46.0.2490.71%2520Safari/537.36'
+            # cs-uri-query
+            line += '\t-'
+            # cs cookie
+            line += '\t-'
+            # x-edge-result-type
+            line += '\tHit'
+            # x-edge-request-id
+            line += '\t' + str(uuid.uuid4())
+
+            stream.write(line + '\n')
 
 if __name__ == '__main__':
     main()
